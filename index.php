153fc90e2fcba7e2ac10e01e48f49d1ed996f3bc
jeffersonbalde <jeffersonbalde8@gmail.com>
2025-11-02 18:17:33 +0800

    TEST

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <?php
-// Temporary router - redirect to main HTML file
-// This forces Digital Ocean to detect PHP
+// Temporary router to detect PHP in DigitalOcean
 if (file_exists(__DIR__ . '/index.html')) {
     include __DIR__ . '/index.html';
 } else {
     echo "CSWDO System - PHP is working!";
 }
 ?>
+
+<?php $version = date('YmdHis'); ?>
+<script src="components/app.js?v=<?php echo $version; ?>"></script>
+<link rel="stylesheet" href="style.css?v=<?php echo $version; ?>">
